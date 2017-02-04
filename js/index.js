a19b3b37671ad55757f0c7ade7fbf9fd944b9488
HYEZ <oohyejioo@gmail.com>
2017-02-05 01:33:13 +0900

    수정

--- js/index.js
+++ js/index.js
@@ -12,6 +12,12 @@ function initTrigger(){
     $("body").on("click", ".page-scroll > a", function(){
 		searchBoxSlide("down");
     });
+    $("body").on("click", ".search-box-more-head", function(){
+		addOtherMeans("down");
+    });
+    $("body").on("click", ".search-box-more-foot", function(){
+		addOtherMeans("up");
+    });
 }
 
 function searchBoxSlide(type){
@@ -26,6 +32,21 @@ function searchBoxSlide(type){
             break;
     }
   
+}
+
+function addOtherMeans(type){
+    switch(type){
+        case "down":
+            $(".search-box-more-form-in").stop(true,true).slideDown(400);
+            $(".search-box-more-head").css("border-bottom-left-radius","0");
+            $(".search-box-more-head").css("border-bottom-right-radius","0");
+            break;
+        case "up":
+            
+            $(".search-box-more-form-in").stop(true,true).slideUp(150);
+            $(".search-box-more-head").css("border-radius","20px");
+            break;
+    }
 }
 
 $(function(){
